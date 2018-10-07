68d2dda56cb1b969561276d9def17c2da13c7bb1
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2018-10-07 23:28:23 +0300

    4.4.2

--- dist/enums.js
+++ dist/enums.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var POSTER_SIZES = [{ id: '30x40cm' }, { id: '50x70cm' }, { id: '70x100cm' }];
+var POSTER_SIZES = [{ id: '30x40cm' }, { id: '50x70cm' }, { id: '70x100cm' }, { id: '12x18inch' }, { id: '18x24inch' }, { id: '24x36inch' }];
 
 var POSTER_ORIENTATIONS = [{ id: 'portrait' }, { id: 'landscape' }];
 
