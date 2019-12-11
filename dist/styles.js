c64c24817ed7a23db27a916dc3c40c8aa8512172
Kimmo Brunfeldt <kimmobrunfeldt@gmail.com>
2019-12-11 12:19:54 +0200

    build

--- dist/styles.js
+++ dist/styles.js
@@ -156,6 +156,12 @@ var MAP_STYLES = [{
   type: 'raster',
   name: 'Black'
 }, {
+  id: 'black-plywood-transparent',
+  color: '#000000',
+  labelColor: '#000000',
+  type: 'raster',
+  name: 'Black'
+}, {
   id: 'copper',
   color: '#DE8E65',
   labelColor: '#DE8E65',
